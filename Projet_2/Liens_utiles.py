96ad77b405ed050bce7586c9ea706422ffacff43
Mathilde <manoe109@ulaval.ca>
2026-03-24 16:25:55 -0400

    Projet 2

---
+++ Projet_2/Liens_utiles.py
@@ -0,0 +1,5 @@
+# Datasheet du capteur DET100A2 Thorlabs (Incluant les courbes de réponse spectrale):
+# https://www.thorlabs.com/free-space-biased-detectors?pn=DET100A2&tabName=Overview
+# Sinon, voir pdf appelé datasheet_det100a2.pdf dans le dossier du projet.
+
+
